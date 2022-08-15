a5531efcfbfea4d29213d4bcb00bf8a775344895
PMPSpadlo <pmp.spadlo@gmail.com>
2022-08-15 22:59:08 +0200

    updateOrederedProduct

--- app/GraphQL/Mutations/UpdateOrderedProduct.php
+++ app/GraphQL/Mutations/UpdateOrderedProduct.php
@@ -15,15 +15,17 @@ final class UpdateOrderedProduct
     public function __invoke($_, array $args)
     {
         $ordered_product = OrderedProduct::where('id',$args['id'])->first();
-        $ordered_product->status = $args['status'];
-        if($args['status'] == 3){
-            $bill = Bill::where('id',$ordered_product->bill_id)->first();
-            $product = Product::where('id',$ordered_product->product_id)->first();
-            $bill->sum -= $product->price;
-            $bill->save();
+        if($ordered_product->status != 3){
+            $ordered_product->status = $args['status'];
+            if($args['status'] == 3){
+                $bill = Bill::where('id',$ordered_product->bill_id)->first();
+                $product = Product::where('id',$ordered_product->product_id)->first();
+                $bill->sum -= $product->price;
+                $bill->save();
 
+            }
+            $ordered_product->save();
         }
-        $ordered_product->save();
         return $ordered_product;
     }
 }
