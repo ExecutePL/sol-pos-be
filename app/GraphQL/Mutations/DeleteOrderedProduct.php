12a88ab8cecfc50e434ba074adcffba860908e03
PMPSpadlo <pmp.spadlo@gmail.com>
2022-08-17 00:38:21 +0200

    OrderedProductDelete

--- app/GraphQL/Mutations/DeleteOrderedProduct.php
+++ app/GraphQL/Mutations/DeleteOrderedProduct.php
@@ -18,7 +18,7 @@ final class DeleteOrderedProduct
         $bill = Bill::where('id',$ordered_product->bill_id)->first();
         $product = Product::where('id',$ordered_product->product_id)->first();
         $bill->sum -= $product->price;
-        $bill->total -= $product->price;
+        $bill->total = round($bill->sum * (1+($bill->tip/100)),2);
         $bill->save();
         $ordered_product->delete();
         return $ordered_product;
