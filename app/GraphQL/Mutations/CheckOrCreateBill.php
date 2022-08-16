080c692fbd50463cbfc20b8b342edbb5956b2ee9
PMPSpadlo <pmp.spadlo@gmail.com>
2022-08-16 15:29:36 +0200

    CheckOrCreateBill

--- app/GraphQL/Mutations/CheckOrCreateBill.php
+++ app/GraphQL/Mutations/CheckOrCreateBill.php
@@ -3,6 +3,8 @@
 namespace App\GraphQL\Mutations;
 
 use App\Models\Bill;
+use App\Models\PointOfSale;
+use App\Models\Worker;
 
 final class CheckOrCreateBill
 {
@@ -14,6 +16,10 @@ final class CheckOrCreateBill
     {
         $bill = Bill::where('pos_id',$args['pos_id'])->where('status',1)->first();
         if(!$bill){
+            $pos = PointOfSale::where('id',$args['pos_id'])->first();
+            $args['user_id'] = $pos->user_id;
+            $worker = Worker::where('user_id',$pos->user_id)->first();
+            $args['worker_id'] = $worker->id;
             $bill = new Bill($args);
             $bill->save();
         }
