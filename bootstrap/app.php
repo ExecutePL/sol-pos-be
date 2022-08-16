0676951038ac74c6d8c1e713f6c1351c8d1bc3ef
katakumby <admin@executeit.pl>
2022-08-16 15:58:49 +0200

    Init front-end

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,9 @@
 $app = new Illuminate\Foundation\Application(
     $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
 );
-
+$app->bind('path.public', function() {
+    return __DIR__."/../build/";
+});
 /*
 |--------------------------------------------------------------------------
 | Bind Important Interfaces
